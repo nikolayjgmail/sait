7530e9e88fefc4a8ab0b20b234722bfdf2e91aa3
nikolayjgmail <nikolyane@gmail.com>
2019-08-01 17:41:07 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import Home from './vew/home/home';
+import About from "./vew/about/about";
+import Work from "./vew/work/work";
+import Process from "./vew/process/process";
+import Services from "./vew/services/services";
+import Testimonials from "./vew/testimonials/testimonials";
+import Contacts from "./vew/contacts/contacts";
+
+
+
+
+class App extends Component{
+
+  render(){
+
+    return(
+        <div>
+            <Home/>
+            <About/>
+             <Work/>
+             <Process/>
+             <Services/>
+             <Testimonials/>
+             <Contacts/>
+        </div>
+    )
+
+
+
+  }
+
+
 }
 
 export default App;
